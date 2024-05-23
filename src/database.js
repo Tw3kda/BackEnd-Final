7744c962a802e362105e52c515d467685325b92e
Tw3kda <santiago2006nc@gmail.com>
2024-05-23 01:10:02 -0500

    fix

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const dbUri = 'mongodb+srv://database:databaseks@daw-db.cxthcz2.mongodb.net/?retryWrites=true&w=majority&appName=DAW-DB://localhost:27017/database'
+const dbUri = process.env.MONGODB_URI
 const connectToDatabase = async () => {
     try {
         await mongoose.connect(dbUri);
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ require('./database')
 
 
 const port =   4000 ;
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
